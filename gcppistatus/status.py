663c297923a9cc941bbe3a4d85b305dde797d3ab
Conor Mulcrone <cmulcrone@gmail.com>
2019-01-09 22:38:20 -0600

    Added functions to retrieve public GCP JSON and tally severity counts

--- gcppistatus/status.py
+++ gcppistatus/status.py
@@ -1,6 +1,39 @@
 import requests
 import json
 
-class jsonstatus:
+class status:
+	def __init__(self, mode):
+		self.mode = mode
+
 	def hello_world(self):
 		return 'hello world'
+
+
+	def getJSON(self, url):
+		response = requests.get(url)
+		status = response.json()
+		return status
+
+	'''
+	Metrics - 
+		- Recency of last event
+		- Severity score
+		- Incident volume (maybe tie to recency)
+		- Product area
+	Visualizations - 
+		- Color
+		- Brightness
+		- Blinking/Strobe/Pulse
+			- Speed
+			- Pattern
+			- Storm
+	'''
+	def calculateMetrics(self, jsontext):
+		severity_scores = {}
+		for status in jsontext:
+			if status['begin']:
+				try:
+					severity_scores[status['severity']] += 1
+				except KeyError:
+					severity_scores[status['severity']] = 1
+		return severity_scores
--- tests/test_status.py
+++ tests/test_status.py
@@ -4,5 +4,6 @@ from gcppistatus.status import *
 class StatusTests(unittest.TestCase):
 
 	def testJSON(self):
-		js = jsonstatus()
-		self.assertEqual(js.hello_world(), 'hello world')
+		ps = status('public')
+		self.assertEqual(ps.hello_world(), 'hello world')
+		self.assertIsInstance(ps.getJSON('https://status.cloud.google.com/incidents.json'), list)
