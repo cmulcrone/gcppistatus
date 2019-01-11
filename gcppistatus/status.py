197a85ee2c860cc5f9102634e2288e709b140c03
Conor Mulcrone <cmulcrone@gmail.com>
2019-01-10 22:29:21 -0600

    Updates to severity scoring

--- gcppistatus/status.py
+++ gcppistatus/status.py
@@ -1,7 +1,14 @@
 import requests
-#import json
+# import json
+
 
 class status:
+
+    severity_weights = {
+        'low': lambda x: x * 1,
+        'medium': lambda x: x * 2,
+        'high': lambda x: x * 3,
+    }
 
     def __init__(self, mode):
         self.mode = mode
@@ -9,16 +16,27 @@ class status:
         self.recency_value = 0
         self.incident_volume = 0
 
-
     def hello_world(self):
         return 'hello world'
-
 
     def getJSON(self, url):
         response = requests.get(url)
         status = response.json()
         return status
 
+    def calculateSeverity(self, jsontext):
+        severity_weights = {
+            'low': lambda x: x * 1,
+            'medium': lambda x: x * 2,
+            'high': lambda x: x * 3,
+        }
+
+
+        severity_score = 0
+        for status in jsontext:
+            if status['severity'] in severity_weights:
+            	severity_score += severity_weights[status['severity']](1)  
+        return severity_score
     '''
     Metrics - 
         - Recency of last event
@@ -33,45 +51,20 @@ class status:
             - Pattern
             - Storm
     '''
-    def calculateMetrics(self, jsontext):
-        severity_scores = {}
-        for status in jsontext:
-            if status['begin']:
-                try: 
-                    severity_scores[status['severity']] += 1
-                except KeyError:
-                    severity_scores[status['severity']] = 1
-        return severity_scores
 
-    severity_scoring = {  
-        'low' : lambda x : x*1,
-        'medium' : lambda x : x*3,
-        'high' : lambda x : x*3,
-    }
+    
 
+    '''
+    try:
+        severity_scores[status['severity']] += 1
+    except KeyError:
+        severity_scores[status['severity']] = 1
+    '''
 
-    def score_player(player):
+    
+    def score_severity(player):
         score = 0
         for stat in player._stats:
             if stat in scoring:
                 score += scoring[stat](getattr(player,stat))    
         return score
-
-    for week in weeks:
-        with open('output/player_score/Week'+str(week)+'.csv', 'w') as csvfile:
-            statwriter = csv.writer(csvfile, delimiter=',', \
-                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            players = nflgame.combine_game_stats(nflgame.games(2017, week)) 
-            for p in players:
-                score = score_player(p)
-                #Writes individual scores of users
-                statwriter.writerow([week, p.playerid.encode(encoding='UTF-8'), \
-                    p.name.encode(encoding='UTF-8'), \
-                    p.guess_position.encode(encoding='UTF_8'), \
-                    round(score,2)])
-                '''print (week, p.playerid.encode(encoding='UTF-8'), \
-                        p.name.encode(encoding='UTF-8'), \
-                        p.guess_position.encode(encoding='UTF_8'), \
-                        round(score,2))
-
-        '''
--- tests/test_status.py
+++ tests/test_status.py
@@ -1,9 +1,11 @@
 import unittest
 from gcppistatus.status import *
 
+
 class StatusTests(unittest.TestCase):
 
-	def testJSON(self):
-		ps = status('public')
-		self.assertEqual(ps.hello_world(), 'hello world')
-		self.assertIsInstance(ps.getJSON('https://status.cloud.google.com/incidents.json'), list)
+    def testJSON(self):
+        ps = status('public')
+        self.assertEqual(ps.hello_world(), 'hello world')
+        self.assertIsInstance(ps.getJSON('https://status.cloud.google.com/incidents.json')
+        , list)
