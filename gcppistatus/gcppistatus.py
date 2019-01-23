23449d7f3363bf70e82f2ac71baa14f4dd57e639
Conor Mulcrone <cmulcrone@gmail.com>
2019-01-23 15:40:52 +0000

    Added GCPPiStatus module

---
+++ gcppistatus/gcppistatus.py
@@ -0,0 +1,17 @@
+import status
+import neopixel
+
+class gcppistatus:
+
+    def __init__(self):
+        self.currentStatus = status('public')
+
+    def runTheCloud(self, currentStatus):
+        try:
+            pass
+        except Exception as e:
+            raise
+        else:
+            pass
+        finally:
+            pass
